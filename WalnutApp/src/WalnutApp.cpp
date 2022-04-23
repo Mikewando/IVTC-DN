a928e697d78ab896e2bf240ab04faa44c6c29d4a
Mikewando <3258334+Mikewando@users.noreply.github.com>
2022-04-23 01:00:51 -0700

    progress

--- WalnutApp/src/WalnutApp.cpp
+++ WalnutApp/src/WalnutApp.cpp
@@ -27,28 +27,96 @@ class ExampleLayer : public Walnut::Layer
 public:
 	virtual void OnUIRender() override
 	{
-		ImGui::Begin("Preview");
-
 		static int error = 0;
-		static int active_frame = 3746;
-		char error_message[1024];
-		if (!error) {
-			const VSFrame* frame = m_VSAPI->getFrame(active_frame, m_Node, error_message, sizeof(error_message));
-			if (!frame) {
-				fprintf(stderr, error_message);
-				error = 1;
-			}
-			const uint8_t* frame_ptr = m_VSAPI->getReadPtr(frame, 0);
-			m_Image->SetData(frame_ptr);
-			m_VSAPI->freeFrame(frame);
+		static char error_message[1024];
+
+		static int active_cycle = 1336;
+		static int last_cycle = -1;
+		bool needNewFrames = last_cycle != active_cycle;
+		last_cycle = active_cycle;
+
+		int max_cycle = m_FieldsFrameCount / 5;
+
+		if (active_cycle < max_cycle && ImGui::IsKeyPressed(ImGuiKey_RightArrow)) {
+			active_cycle++;
 		}
-		ImGui::Image(m_Image->GetDescriptorSet(), { (float)m_Width, (float)m_Height });
+		if (active_cycle > 0 && ImGui::IsKeyPressed(ImGuiKey_LeftArrow)) {
+			active_cycle--;
+		}
+		ImGui::Begin("Fields");
+
+		for (int i = 0; i < 5; i++) {
+			if (needNewFrames && !error) {
+				const VSFrame* frame = m_VSAPI->getFrame(active_cycle * 5 + i, m_FieldsNode, error_message, sizeof(error_message));
+				if (!frame) {
+					fprintf(stderr, error_message);
+					error = 1;
+				}
+				const uint8_t* frame_ptr = m_VSAPI->getReadPtr(frame, 0);
+				m_Fields[i]->SetData(frame_ptr);
+				m_VSAPI->freeFrame(frame);
+			}
+		}
+		for (int i = 0; i < 5; i++) {
+			ImGui::Image(m_Fields[i]->GetDescriptorSet(), { (float)m_FieldsWidth, (float)m_FieldsHeight });
+			if (i < 4) {
+				ImGui::SameLine();
+			}
+		}
+
+		ImGui::End();
+
+		ImGui::Begin("Output");
+
+		for (int i = 0; i < 4; i++) {
+			if (needNewFrames && !error) {
+				const VSFrame* frame = m_VSAPI->getFrame(active_cycle * 4 + i, m_FramesNode, error_message, sizeof(error_message));
+				if (!frame) {
+					fprintf(stderr, error_message);
+					error = 1;
+				}
+				const uint8_t* frame_ptr = m_VSAPI->getReadPtr(frame, 0);
+				m_Frames[i]->SetData(frame_ptr);
+				m_VSAPI->freeFrame(frame);
+			}
+		}
+		for (int i = 0; i < 4; i++) {
+			ImGui::Image(m_Frames[i]->GetDescriptorSet(), { (float)m_FramesWidth, (float)m_FramesHeight });
+			if (i < 4) {
+				ImGui::SameLine();
+			}
+		}
 
 		ImGui::End();
 
 		ImGui::Begin("Controls");
-		ImGui::SliderInt("Active Frame", &active_frame, 0, m_FrameCount - 1);
+		ImGui::SliderInt("Active Cycle", &active_cycle, 0, max_cycle);
 		ImGui::SameLine(); HelpMarker("CTRL+click to input value.");
+		static bool frames[10] = {true, true, true, true, false, true, true, false, true, true};
+		int active_count = 0;
+		for (int i = 0; i < 10; i++) {
+			if (frames[i]) {
+				active_count++;
+			}
+			if (active_count > 8) {
+				frames[i] = false;
+			}
+		}
+
+		// Top Fields
+		ImGui::Checkbox("0", &frames[0]); ImGui::SameLine();
+		ImGui::Checkbox("2", &frames[2]); ImGui::SameLine();
+		ImGui::Checkbox("4", &frames[4]); ImGui::SameLine();
+		ImGui::Checkbox("6", &frames[6]); ImGui::SameLine();
+		ImGui::Checkbox("8", &frames[8]);
+
+		// Bottom Fields
+		ImGui::Checkbox("1", &frames[1]); ImGui::SameLine();
+		ImGui::Checkbox("3", &frames[3]); ImGui::SameLine();
+		ImGui::Checkbox("5", &frames[5]); ImGui::SameLine();
+		ImGui::Checkbox("7", &frames[7]); ImGui::SameLine();
+		ImGui::Checkbox("9", &frames[9]);
+
 		ImGui::End();
 		ImGui::ShowDemoWindow();
 	}
@@ -68,50 +136,82 @@ public:
 
 		m_ScriptEnvironment = m_VSSAPI->createScript(nullptr);
 
-		set_active_file("A:\\Subs\\todo\\flcl\\01\\walnut.vpy");
+		set_active_fields("A:\\Subs\\todo\\flcl\\01\\walnut\\separate_fields.vpy");
+		set_active_frames("A:\\Subs\\todo\\flcl\\01\\walnut\\output frames.vpy");
 	}
 
-	void set_active_file(const char* file) {
+	void set_active_fields(const char* file) {
 		int error = m_VSSAPI->evaluateFile(m_ScriptEnvironment, file);
 		if (error != 0) {
 			fprintf(stderr, "Error loading file: %s\n", m_VSSAPI->getError(m_ScriptEnvironment));
 		}
 
-		m_Node = m_VSSAPI->getOutputNode(m_ScriptEnvironment, 0);
-		m_VI = m_VSAPI->getVideoInfo(m_Node);
-		fprintf(stderr, "Video Width: %d x %d\n", m_VI->width, m_VI->height);
-		fprintf(stderr, "Video Format: %d, %d, %d\n", m_VI->format.colorFamily, m_VI->format.bitsPerSample, m_VI->format.numPlanes);
-		m_Width = m_VI->width;
-		m_Height = m_VI->height;
-		m_FrameCount = m_VI->numFrames;
+		m_FieldsNode = m_VSSAPI->getOutputNode(m_ScriptEnvironment, 0);
+		const VSVideoInfo* vi = m_VSAPI->getVideoInfo(m_FieldsNode);
+		fprintf(stderr, "Video Width: %d x %d\n", vi->width, vi->height);
+		fprintf(stderr, "Video Format: %d, %d, %d\n", vi->format.colorFamily, vi->format.bitsPerSample, vi->format.numPlanes);
+		m_FieldsWidth = vi->width;
+		m_FieldsHeight = vi->height;
+		m_FieldsFrameCount = vi->numFrames;
 
-		m_Image = std::make_shared<Walnut::Image>(
-			m_Width,
-			m_Height,
-			Walnut::ImageFormat::RGBA,
-			nullptr);
+		for (int i = 0; i < 5; i++) {
+			m_Fields[i] = std::make_shared<Walnut::Image>(
+				m_FieldsWidth,
+				m_FieldsHeight,
+				Walnut::ImageFormat::RGBA,
+				nullptr);
+		}
+	}
+
+	void set_active_frames(const char* file) {
+		int error = m_VSSAPI->evaluateFile(m_ScriptEnvironment, file);
+		if (error != 0) {
+			fprintf(stderr, "Error loading file: %s\n", m_VSSAPI->getError(m_ScriptEnvironment));
+		}
+
+		m_FramesNode = m_VSSAPI->getOutputNode(m_ScriptEnvironment, 0);
+		const VSVideoInfo* vi = m_VSAPI->getVideoInfo(m_FramesNode);
+		fprintf(stderr, "Video Width: %d x %d\n", vi->width, vi->height);
+		fprintf(stderr, "Video Format: %d, %d, %d\n", vi->format.colorFamily, vi->format.bitsPerSample, vi->format.numPlanes);
+		m_FramesWidth = vi->width;
+		m_FramesHeight = vi->height;
+		m_FramesFrameCount = vi->numFrames;
+
+		for (int i = 0; i < 4; i++) {
+			m_Frames[i] = std::make_shared<Walnut::Image>(
+				m_FramesWidth,
+				m_FramesHeight,
+				Walnut::ImageFormat::RGBA,
+				nullptr);
+		}
 		m_ActiveFile = file;
 	}
+
 
 private:
 	const VSAPI* m_VSAPI = nullptr;
 	const VSSCRIPTAPI* m_VSSAPI = nullptr;
 	VSScript* m_ScriptEnvironment = nullptr;
-	VSNode* m_Node = nullptr;
-	const VSVideoInfo* m_VI = nullptr;
-	int m_Width = 0;
-	int m_Height = 0;
-	int m_FrameCount = 0;
-	const char* m_ActiveFile;
-	std::shared_ptr<Walnut::Image> m_Image;
-
+	const char* m_ActiveFile; // TODO
+	// Fields
+	VSNode* m_FieldsNode = nullptr;
+	int m_FieldsWidth = 0;
+	int m_FieldsHeight = 0;
+	int m_FieldsFrameCount = 0;
+	std::shared_ptr<Walnut::Image> m_Fields[5];
+	// Frames
+	VSNode* m_FramesNode = nullptr;
+	int m_FramesWidth = 0;
+	int m_FramesHeight = 0;
+	int m_FramesFrameCount = 0;
+	std::shared_ptr<Walnut::Image> m_Frames[4];
 };
 
 std::shared_ptr<ExampleLayer> g_Layer = nullptr;
 
 void glfw_drop_callback(GLFWwindow* window, int path_count, const char* paths[]) {
 	fprintf(stderr, "Path count: %d, First Path: %s\n", path_count, paths[0]);
-	g_Layer->set_active_file(paths[0]);
+	g_Layer->set_active_frames(paths[0]);
 	return;
 }
 
