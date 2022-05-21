ba9e7f7b6ca589765f12674ba46b63fd4f53b501
Mikewando <3258334+Mikewando@users.noreply.github.com>
2022-05-21 13:58:44 -0700

    Additional premake stuff

---
+++ vendor/libp2p/premake5.lua
@@ -0,0 +1,37 @@
+project "libp2p"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"p2p.h",
+		"p2p_api.h",
+		"p2p_api.cpp",
+		"v210.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
---
+++ vendor/miniz/premake5.lua
@@ -0,0 +1,35 @@
+project "miniz"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"miniz.h",
+		"miniz.c"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
